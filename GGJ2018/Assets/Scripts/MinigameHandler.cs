b7b34d9e64806e13cc0e76d77bab2c113464c736
Wolly <wolly.m.b@gmail.com>
2018-01-28 02:45:56 +0100

    Randomized letters, changed prototype scene to better reflect the actual game scene. Bars still not there.

--- GGJ2018/Assets/Scripts/MinigameHandler.cs
+++ GGJ2018/Assets/Scripts/MinigameHandler.cs
@@ -12,7 +12,7 @@ public class MinigameHandler : MonoBehaviour {
     [SerializeField] private float _scrollSpeed;
 
     private GameObject[] _letters = null;
-    private List<GameObject> _chunkArray = null;
+    private List<GameObject> _chunkList = null;
     private RectTransform _hitBoxRect;
     private EventSystem _eventSystem;
 
@@ -20,33 +20,47 @@ public class MinigameHandler : MonoBehaviour {
     private bool _chunkSelected = false;
     private int _index = 0;
 
-	// Use this for initialization
-	void Start () {
+    private string uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public List<GameObject> ChunkList {
+        get { return _chunkList; }
+    }
+
+    // Use this for initialization
+    void Start () {
         _eventSystem = GameObject.FindGameObjectWithTag("EvtSystem").GetComponent<EventSystem>();
-        _startScrolling = true;
         _letters = new GameObject[_content.transform.childCount];
-        _chunkArray = new List<GameObject>(_chunks.transform.childCount);
+        _chunkList = new List<GameObject>(_chunks.transform.childCount);
 
-        for (int i = 0; i < _letters.Length; i++) {
-            _letters[i] = _content.transform.GetChild(i).gameObject;
-        }
-
-        for (int i = 0; i < _chunkArray.Count; i++) {
-            _chunkArray[i] = _chunks.transform.GetChild(i).gameObject;
+        for (int i = 0; i < _chunkList.Count; i++) {
+            _chunkList[i] = _chunks.transform.GetChild(i).gameObject;
         }
 
         _hitBoxRect = _hitBox.GetComponent<RectTransform>();
+
     }
 	
+    private void RandomizeLetters() {
+        for (int i = 0; i < _letters.Length; i++) {
+            _letters[i] = _content.transform.GetChild(i).gameObject;
+
+            string randomChar = uppercaseAlphabet[Random.Range(0, uppercaseAlphabet.Length)].ToString();
+            _letters[i].GetComponent<Text>().text = randomChar;
+        }
+    }
+
     public void SelectChunk() {
         if (!_chunkSelected) {
             _chunkSelected = true;
+            _startScrolling = true;
+            RandomizeLetters();
         }
     }
 
 	// Update is called once per frame
 	void Update () {
         if (_chunkSelected) {
+            Debug.Log("Calling chunky monkey");
             DisableChunkInteraction();
             ScrollText();
         }
@@ -56,8 +70,8 @@ public class MinigameHandler : MonoBehaviour {
         if (_eventSystem.currentSelectedGameObject != null) {
             _eventSystem.SetSelectedGameObject(null);
 
-            for (int i = 0; i < _chunkArray.Count; i++) {
-                Button chunk = _chunkArray[i].GetComponent<Button>();
+            for (int i = 0; i < _chunkList.Count; i++) {
+                Button chunk = _chunkList[i].GetComponent<Button>();
                 chunk.interactable = false;
             }
         }
@@ -71,6 +85,8 @@ public class MinigameHandler : MonoBehaviour {
                 _startScrolling = false;
                 _chunkSelected = false;
                 _index = 0;
+                _horizontalScrollBar.value = 0;
+                _eventSystem.SetSelectedGameObject(_chunks.transform.GetChild(0).gameObject);
             } else {
                 if (!HasEnded()) {
                     HandleKeyHit();
@@ -90,11 +106,11 @@ public class MinigameHandler : MonoBehaviour {
 
         if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Return)) {
             if (Input.GetKeyDown(_letters[_index].GetComponent<Text>().text.ToLower())) {
-                if (_hitBoxRect.transform.position.x - 1.0f < letter.transform.position.x
-                    && _hitBoxRect.transform.position.x + 1.5f > letter.transform.position.x) {
+                if (_hitBoxRect.transform.position.x - 150.0f < letter.transform.position.x
+                    && _hitBoxRect.transform.position.x + 100f > letter.transform.position.x) {
                     Debug.Log("Scored a point! How great..");
                     _index++;
-                } else if (_hitBoxRect.transform.position.x + 3.0f < letter.transform.position.x) {
+                } else if (_hitBoxRect.transform.position.x + 190.0f < letter.transform.position.x) {
                     Debug.Log("Pressed the button too early!");
                 }
             } else {
@@ -102,7 +118,7 @@ public class MinigameHandler : MonoBehaviour {
             }
         }
 
-        if (_hitBoxRect.transform.position.x - 1.0f > letter.transform.position.x) {
+        if (_hitBoxRect.transform.position.x - 80.0f > letter.transform.position.x) {
             Debug.Log("Pressed too late...");
             _index++;
         }
---
+++ GGJ2018/Assets/Scripts/RadarLineBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class RadarLineBehaviour : MonoBehaviour {
+    [SerializeField] private GameObject _radarLine;
+    [SerializeField] private GameObject _chunks;
+    [SerializeField] private float _speed;
+
+    private RectTransform _maskRect;
+    private RectTransform _lineRect;
+    private Vector3 _startPosition;
+
+	// Use this for initialization
+	void Start () {
+        _maskRect = GetComponent<RectTransform>();
+        _lineRect = _radarLine.GetComponent<RectTransform>();
+
+        _startPosition = _lineRect.anchoredPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+            if (Mathf.Abs(_lineRect.anchoredPosition.y) < _maskRect.rect.height) {
+                _radarLine.transform.position = new Vector3(_radarLine.transform.position.x, _radarLine.transform.position.y - _speed * Time.deltaTime, _radarLine.transform.position.z);
+                DetectChunkCollision();
+            } else {
+                _lineRect.anchoredPosition = _startPosition;
+            }
+    }
+
+    private void DetectChunkCollision() {
+        for (int i = 0; i < _chunks.transform.childCount; i++) {
+            GameObject chunk = _chunks.transform.GetChild(i).gameObject;
+            RectTransform rectTransform = chunk.GetComponent<RectTransform>();
+
+            if (_radarLine.transform.position.y > chunk.transform.position.y - rectTransform.rect.height / 2 + _lineRect.rect.height / 2 
+                && _radarLine.transform.position.y < chunk.transform.position.y - _lineRect.rect.height / 2) {
+                // Update chunky monkey sprite..
+                // chunk.GetComponent<Image>().sprite = newSprite;
+                chunk.GetComponent<Image>().color = new Color(Mathf.Sin(Time.time), Mathf.Sin(Time.time), Mathf.Sin(Time.time));
+            }
+        }
+    }
+}
